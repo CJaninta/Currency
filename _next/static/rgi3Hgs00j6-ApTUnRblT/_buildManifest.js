aa92078ef0067920d7c2d772aa3b08161750028c
CJaninta <52588730+CJaninta@users.noreply.github.com>
2021-01-31 15:25:04 +0000

    Deploying to gh-pages from  @ a486ef94cfb670c4d0d28435c4261fd20d98f30b 🚀

---
+++ _next/static/rgi3Hgs00j6-ApTUnRblT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fbe34c70c38a2d61ca504.css","static\u002Fchunks\u002Fpages\u002Findex-8ad130c16186c29ccf8b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
